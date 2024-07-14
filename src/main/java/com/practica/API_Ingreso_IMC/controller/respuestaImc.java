990243376d9f8964449014e75523c36e210e701a
DAVID <davidpintodev97@gmail.com>
2024-07-14 14:29:59 +0200

    creación metodo para calcular IMC de una persona

--- src/main/java/com/practica/API_Ingreso_IMC/controller/respuestaImc.java
+++ src/main/java/com/practica/API_Ingreso_IMC/controller/respuestaImc.java
@@ -1,5 +1,19 @@
 package com.practica.API_Ingreso_IMC.controller;
 
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+/*
+ * Controller API; respuesta cáculo a partir de Ingreso de IMC
+ */
+@RestController
 public class respuestaImc {
+	/**
+	 * Calcular IMC según x parametros de entrada en solictud GET
+	 * @return: Estado del peso de la persona
+	 */
+	public String calcularIMC(@RequestParam double peso, @RequestParam double estaturaMetros) {
+		return null;
+		
+	}
 
 }
